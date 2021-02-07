10c0127fc23a6dba2cc216b6842d54f091056a9c
Vicoj <vico.jans@gmail.com>
2021-02-07 17:44:00 +0100

    nex sale tool

---
+++ BoVeloManager/tools/Sales.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVeloManager.tools
+{
+    class Sales
+    {
+        static private string salesId = "";
+        public static string getSalesId()
+        {
+            return salesId;
+        }
+    }
+}
