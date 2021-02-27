c7b0588eff78e690181ca1b9d047fd0489db64b7
Lebecque <Florianlebecque@hotmail.com>
2021-02-27 15:32:46 +0100

    Migration to new arch

---
+++ BoVeloManager/Classes/human.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVeloManager.Classes
+{
+    class human
+    {
+        private readonly int id;
+        private string name;
+
+
+       
+    }
+}
---
+++ BoVeloManager/Classes/user.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVeloManager.Classes
+{
+    class user : human
+    {
+        private readonly int id;
+        private string name;
+        private int grade;
+
+
+        public user(int id_, string name_) {
+
+            name = name_;
+        }
+
+        public  string getUserName()
+        {
+            return name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public int getGrade(){
+            return grade;
+        }
+
+        public static void setGrade(int number){
+
+        }
+
+    }
+}
