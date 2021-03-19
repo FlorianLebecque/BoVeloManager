e370d44e4fe0b998ebf5d45602859dd18c649557
Florian <Florianlebecque@hotmail.com>
2021-03-19 15:46:11 +0100

    Removed unuse queries

--- BoVeloManager/tools/database.cs
+++ BoVeloManager/tools/database.cs
@@ -96,14 +96,6 @@ namespace BoVeloManager.tools {
 
     class DatabaseQuery {
 
-        // Management Querry
-        public static string getUserPass(string user) {
-            return "SELECT `psw` FROM `bv_user` WHERE `user` = '" + user + "'";
-        }
-
-        public static string getUserData_byName(string user) {
-            return "SELECT `id`,`grade` FROM `bv_user` WHERE `user` = '" + user + "'";
-        }
 
         public static string getUsers(int filter) {
             string f = "";
@@ -125,215 +117,88 @@ namespace BoVeloManager.tools {
 
             return "SELECT * FROM `bv_user`" + f;
         }
-        public static string getUserGrade_by_id(int id)
-        {
-            return "SELECT `grade` FROM `bv_user` WHERE `id` = " + id.ToString();
-        }
-        public static string getUser_by_id(int id) {
-            return "SELECT `id`,`user`, `grade` FROM `bv_user` WHERE `id` = " + id.ToString();
-        }
 
         public static string addUser(string name, string pass, int grade) {
             return "INSERT INTO `bv_user`(`user`, `psw`, `grade`) VALUES ('" + name + "','" + pass + "'," + grade.ToString() + ")";
-        }
-
-
-        public static string setUserGrade(int id, int grade) {
-            return "UPDATE `bv_user` SET `grade`= " + grade.ToString() + " WHERE `id` = " + id.ToString();
-        }
-
-        public static string updateClient(int id, string entreprise_name, string enterprise_adress, string email, string phone_num) {
-            return "UPDATE `bv_client` SET `enterprise_name`= '" + entreprise_name + "', `enterprise_adress`= '" + enterprise_adress + "',  `email`= '" + email + "', `phone_num`= '" + phone_num + "' WHERE `id` = " + id.ToString();
         }
 
         public static string setUserPass(int id, string pass) {
             return "UPDATE `bv_user` SET `psw`='" + pass + "'  WHERE `id` =" + id.ToString();
         }
 
-        public static string delUser(int id) {
-            return "DELETE FROM `bv_user` WHERE `id` = " + id.ToString();
+        public static string setUserGrade(int id, int grade) {
+            return "UPDATE `bv_user` SET `grade`= " + grade.ToString() + " WHERE `id` = " + id.ToString();
         }
 
-        #region Item
+
+
+        public static string updateClient(int id, string entreprise_name, string enterprise_adress, string email, string phone_num) {
+            return "UPDATE `bv_client` SET `enterprise_name`= '" + entreprise_name + "', `enterprise_adress`= '" + enterprise_adress + "',  `email`= '" + email + "', `phone_num`= '" + phone_num + "' WHERE `id` = " + id.ToString();
+        }
+
+        public static string getClients() {
+            return "SELECT `id`,`first_name`, `last_name`, `enterprise_name`, `enterprise_adress`, `email`, `phone_num` , `date` FROM `bv_client`";
+        }
+
+        public static string addClient(Client c) {
+            return "INSERT INTO `bv_client`(`id`,`first_name`, `last_name`, `enterprise_name`, `enterprise_adress`, `email`, `phone_num`,`date`) VALUES (" + c.getId() + ",'" + c.getName() + "',' ','" + c.getEtpName() + "','" + c.getEtpAdress() + "','" + c.getEmail() + "','" + c.getPhoneNumb() + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "')";
+        }
+
+
+
+        public static string getCatalogBike() {
+            return "SELECT * FROM `bv_catalog`";
+        }
+
+        public static string addCatalogBike(CatalogBike cb) {
+            return "INSERT INTO `bv_catalog` (`id`,`name`,`PriceMul`,`picture`) VALUES (" + cb.getId().ToString() + ",'" + cb.getName() + "'," + cb.getPriceMul().ToString() + ", 'Bike0.jpg')";
+        }
+
+        public static string updateCatalogBike(CatalogBike kt) {
+            return "UPDATE `bv_catalog` SET `name` = '" + kt.getName() + "' , `PriceMul` = '" + kt.getPriceMul().ToString() + "' WHERE `id` = " + kt.getId().ToString();
+        }
+
+
 
         public static string getKits() {
             return "SELECT * FROM `bv_type_kit`";
         }
 
-        public static string getKits_maxId(int maxId)
-        {
-            return "SELECT * FROM `bv_type_kit` WHERE `id` < " + maxId.ToString();
-        }
-
-        public static string getItem()
-        {
-            return "SELECT * FROM `bv_catalog`";
-        }
-        public static string getItem_by_id(int id)
-        {
-            return "SELECT `id`,`name`,`PriceMul` FROM `bv_catalog` WHERE `id` = " + id.ToString();
-        }
-        public static string addCatalogBike(CatalogBike cb)
-        {
-            return "INSERT INTO `bv_catalog` (`id`,`name`,`PriceMul`,`picture`) VALUES (" + cb.getId().ToString() + ",'" + cb.getName() + "'," + cb.getPriceMul().ToString() + ", 'Bike0.jpg')";
-        }
-
-        public static string updateCatalogBike(CatalogBike kt)
-        {
-            return "UPDATE `bv_catalog` SET `name` = '" + kt.getName() + "' , `PriceMul` = '" + kt.getPriceMul().ToString() + "' WHERE `id` = " + kt.getId().ToString();
-        }
-
-        public static string delItem(int id)
-        {
-            return "DELETE FROM `bv_catalog` WHERE `id` = " + id.ToString();
-        }
-
-        #endregion
-
-        #region Kit
-        public static string getKit_by_category(int cat)
-        {
-            return "SELECT * FROM `bv_type_kit` WHERE `category` = " + cat.ToString();
-        }
-
-        public static string getKit_by_catalogBikeId(int id_cat)
-        {
+        public static string getKit_by_catalogBikeId(int id_cat) {
             return "SELECT `id_tKit` FROM `bv_cat_tKit` WHERE `id_cat` = " + id_cat.ToString();
         }
 
-        public static string getCompatibleCatId_with_KitId(int id_kit)
-        {
-            return "SELECT `id_cat` FROM `bv_cat_tKit` WHERE `id_tKit` = " + id_kit.ToString();
-        }
-
-        public static string getKit_by_id(int id)
-        {
-            return "SELECT `id`,`name`,`properties`,`category`,`price` FROM `bv_type_kit` WHERE `id` = " + id.ToString();
-        }
-
-        public static string delKit(int id)
-        {
-            return "DELETE FROM `bv_type_kit` WHERE `id` = " + id.ToString();
-        }
-
-        public static string setKitName(int id, string name)
-        {
-            return "UPDATE `bv_type_kit` SET `name` = '" + name + "' WHERE `id` = " + id.ToString();
-        }
-
-        public static string setKitProperties(int id, string newProperties)
-        {
-            return "UPDATE `bv_type_kit` SET `properties` = '" + newProperties + "' WHERE `id` = " + id.ToString();
-        }
-
-        // get all kit info
-        public static string gettKit(int id_tBike)
-        {
+        public static string getKitId_byTBike(int id_tBike) {
             return "SELECT K.id FROM `bv_tBike_tKit` AS B INNER JOIN `bv_type_kit` AS K ON B.id_tKit = K.id WHERE B.id_tBike =" + id_tBike.ToString();
         }
 
-        // Add kit Querry
-        public static string addKit(int id,string name, string prop,int price, int cat)
-        {
+        public static string addKit(int id,string name, string prop,int price, int cat) {
             return "INSERT INTO `bv_type_kit`(`id`,`name`, `properties`,`price`, `category`) VALUES ('" + id.ToString() + "','" + name + "','" + prop + "','" + price.ToString() + "','" + cat.ToString() + "')";
         }
-        public static string addCompatibleKit(int id_cat, int id_tKit)
-        {
+
+        public static string addCompatibleKit(int id_cat, int id_tKit) {
             return "INSERT INTO `bv_cat_tKit` (`id_cat`, `id_tKit`) VALUES('" + id_cat + "', '" + id_tKit + "')";
         }
-        ////////////////////////
-        public static string delCompatibleKit(int id_cat, int id_tKit)
-        {
-            ///REQUETE SQL A ECRIRE
-            //return "DELETE FROM `bv_cat_tKit` WHERE id_cat = " + id_cat.ToString() + "AND id_tKit = " + id_tKit.ToString();
+
+        public static string delCompatibleKit(int id_cat, int id_tKit) {
             return "DELETE FROM `bv_cat_tKit` WHERE `bv_cat_tKit`.`id_cat` = " + id_cat.ToString() + " AND `bv_cat_tKit`.`id_tKit` = " + id_tKit.ToString();
         }
-        ///////////////////////
 
-        //VOIR INNER JOIN POUR AFFICHER LES KITS
-        //https://sql.sh/cours/jointures/inner-join
-        #endregion
 
-        // Sales Querry
 
-        //returns all sales from the shop
         public static string getSales() {
             return "SELECT `id` ,`id_client`, `id_seller`, `state`, `prevision_date`, `date` FROM `bv_sale`";
-        }
-        public static string getOldSales()
-        {
-            return "SELECT S.id,S.state, CONCAT(`first_name` , ' ', `last_name`) AS Client, S.date FROM `bv_sale` AS S INNER JOIN `bv_client` AS C ON S.id_client = C.id INNER JOIN `bv_user` AS U ON S.id_seller = U.id";
-        }
-        // Returns the sale_id the Client fullname the sale date
-        public static string getSale_by_id(int id)
-        {
-            return "SELECT S.id ,CONCAT(`first_name` , ' ', `last_name`)  AS Client, U.user , C.enterprise_name, S.date FROM `bv_sale` AS S INNER JOIN `bv_client` AS C ON S.id_client = C.id INNER JOIN `bv_user` AS U ON U.id = S.id_seller  WHERE S.id = " + id.ToString(); //INNER JOIN `bv_seller` AS SE ON S.id_seller = SE.id
-        }
-        //Returns all types of bikes from one sale
-        public static string gettBikes_by_sale(int id_sale)
-        {
-            return "SELECT id_tBike , qnt , PriceMul, name FROM `bv_sale_bike` as SB INNER JOIN bv_type_bike AS TB ON TB.id = SB.`id_tBike` INNER JOIN bv_catalog AS C ON TB.id_cat = C.id WHERE id_sale = " + id_sale.ToString();
-        }
-        // Returns id name price from a type of bike
-        public static string gettBike(int id_Bike)
-        {
-            return "SELECT id FROM `bv_type_bike`  WHERE id = " + id_Bike.ToString();
-        }
-
-        public static string getClients()
-        {
-            return "SELECT `id`,`first_name`, `last_name`, `enterprise_name`, `enterprise_adress`, `email`, `phone_num` , `date` FROM `bv_client`";
-        }
-
-        public static string getClient_by_id(int id)
-        {
-            return "SELECT `id`,`first_name`, `last_name`, `enterprise_name`, `enterprise_adress`, `email`, `phone_num` , `date` FROM `bv_client` WHERE `id` = " + id.ToString();
-        }
-
-
-        public static string addClient(Client c)
-        {
-            return "INSERT INTO `bv_client`(`id`,`first_name`, `last_name`, `enterprise_name`, `enterprise_adress`, `email`, `phone_num`,`date`) VALUES (" + c.getId() + ",'" + c.getName() + "',' ','" + c.getEtpName() + "','" + c.getEtpAdress() + "','" + c.getEmail() + "','" + c.getPhoneNumb() + "','" + DateTime.Now.ToString("yyyy-MM-dd") + "')";
-        }
-
-        // Get available Kits
-        #region Kits
-        public static string getAllKits()
-        {
-            return "SELECT * FROM `bv_type_kit`";
-        }
-        public static string getNameOfKitById(int Id)
-        {
-            return "SELECT * FROM `bv_type_kit` WHERE `id` = " + Id.ToString();
-        }
-        
-        public static string getColor()
-        {
-            return "SELECT * FROM `bv_type_kit` WHERE `category` = 1";
-        }
-
-        public static string getSize()
-        {
-            return "SELECT * FROM `bv_type_kit` WHERE `category` = 0";
-        }
+        } 
 
         public static string updateKitTemplate(int id,string name,int cat,int price,string prop) {
             return "UPDATE `bv_type_kit` SET `name`= '"+name+"',`category`='"+cat.ToString()+ "',`Price`='"+price.ToString()+"',`properties`='" + prop + "' WHERE `id`=" + id;
         }
-
-        #endregion 
-
-        #region Sale
 
         public static string addSale(Sale s)
         {
             return "INSERT INTO `bv_sale`(`id`, `id_client`, `id_seller`,`state`, `prevision_date`, `date`) VALUES ('" + s.getId() + "', '" + s.getClient().getId() + "','" + s.getSeller().getId() + "','Open','" + s.getPreSaleDate().ToString("yyyy-MM-dd") + "','" + s.getSaleDate().ToString("yyyy-MM-dd") + "')";
         }
 
-        public static string link_sale_to_Sale_bike(Sale s, Bike b)
-        {
-            return "";
-        }
 
         public static string addBikeTemplate(BikeTemplate t)
         {
@@ -345,25 +210,16 @@ namespace BoVeloManager.tools {
             //return "INSERT INTO `bv_bike` (`id`, `id_tBike`, `id_sale`, `state`, `planne_cDate`, `poste`) VALUES (" + b.getId() + ", " + b.getBikeTempl().getId() + ", " + b.getSaleId() + ", " + b.getState() + ", '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', '" + b.getPoste() + ")";
             if(b.getSaleId() >= 0){
                 return "INSERT INTO `bv_bike` (`id`, `id_tBike`, `id_sale`, `state`, `planne_cDate`, `create_Date`, `poste`) VALUES (" + b.getId() + ", " + b.getBikeTempl().getId() + ", " + b.getSaleId() + ", " + b.getState() + ", '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', " + b.getPoste() + ")";
-            }
-            else
-            {
-                return "INSERT INTO `bv_bike` (`id`, `id_tBike`, `state`, `planne_cDate`, `create_Date`, `poste`) VALUES (" + b.getId() + ", " + b.getBikeTempl().getId() + ", " + b.getState() + ", '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', " + b.getPoste() + ")";
-                
+            } else {
+                return "INSERT INTO `bv_bike` (`id`, `id_tBike`, `state`, `planne_cDate`, `create_Date`, `poste`) VALUES (" + b.getId() + ", " + b.getBikeTempl().getId() + ", " + b.getState() + ", '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', '" + b.getPlannedtDate().ToString("yyyy-MM-dd") + "', " + b.getPoste() + ")";  
             }
         }
         
-        public static string link_kit_to_tbike(BikeTemplate bt, KitTemplate kt)
-        {
+        public static string link_kit_to_tbike(BikeTemplate bt, KitTemplate kt) {
             return "INSERT INTO `bv_tBike_tKit` (`id_tBike`, `id_tKit`) VALUES (" + bt.getId() + ", " + kt.getId() + ")";
         }
 
-
-        #endregion
-
-        //bike
-        public static string getBike()
-        {
+        public static string getBike() { 
             return "SELECT * FROM `bv_bike`";
         }
 
@@ -374,19 +230,7 @@ namespace BoVeloManager.tools {
         public static string getTBike() {
             return "SELECT * FROM `bv_type_bike`";
         }
-        //public static string addBike(int id, int status, int id_sale, int Poste, BikeTemplate bt, DateTime planned_date, DateTime constr_date)
-        //{
-            //if (id_sale < 0)
-            //{
-            //    string str = "NULL";
-            //    return "INSERT INTO `bv_bike`(`id`, `id_tBike`, `id_sale`, `state`, `poste`, `planne_cDate`, `create_Date`) VALUES ('" + id + "','" + bt.getId() + "','" + str + "','" + status + "','" + Poste + "','" + planned_date.ToString("yyyy-MM-dd") + "','" + constr_date.ToString("yyyy-MM-dd") + "')";
-            //}
-            //else
-            //{
-                //return "INSERT INTO `bv_bike`(`id`, `id_tBike`, `id_sale`, `state`, `planne_cDate`, `create_Date`, `poste`) VALUES ('" + id + "','" + bt.getId() + "','" + id_sale + "','" + status + "','" + planned_date.ToString("yyyy-MM-dd") + "','" + constr_date.ToString("yyyy-MM-dd") + "','" + Poste.ToString() + "')";
-            //}   
-                
-        //}
+
 
     }
 
@@ -635,7 +479,7 @@ namespace BoVeloManager.tools {
 
                         BikeTemplate bt = new BikeTemplate(id, cb);
 
-                        string q = DatabaseQuery.gettKit(id);
+                        string q = DatabaseQuery.getKitId_byTBike(id);
                         DataTable kdt = Database.getData(q);
 
                         for(int j = 0; j < kdt.Rows.Count; j++) {
@@ -678,7 +522,7 @@ namespace BoVeloManager.tools {
 
         public static List<CatalogBike> getCatalogBikes(List<KitTemplate> ktList) {
 
-            string q = DatabaseQuery.getItem();
+            string q = DatabaseQuery.getCatalogBike();
             DataTable dt = Database.getData(q);
 
             //convert all the user into a user object
