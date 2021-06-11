72b870c516eb84fed15b394718cdea28e9f88a62
195344 <60741544+195344@users.noreply.github.com>
2021-06-11 16:37:51 +0200

    AB#142 add qt stock

--- BoVeloManager/Classes/KitTemplate.cs
+++ BoVeloManager/Classes/KitTemplate.cs
@@ -14,7 +14,8 @@ namespace BoVeloManager.Classes {
         private string properties;
 
         private int stock_qtt;
-        private int stock_location;
+        private int stock_location_alley_number;
+        private int stock_location_crate_number;
 
         public KitTemplate(int id_,string name_,int cat_,int price_,string prop_) {
             id = id_;
