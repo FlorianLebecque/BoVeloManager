c8f2fd36fbd6d6397731ad033aff50374ad37609
Robin Lamotte <17051@ecam.be>
2021-06-12 13:45:39 +0200

    outil de recherche

--- BoVeloManager/Classes/Controler/Sale_controler.cs
+++ BoVeloManager/Classes/Controler/Sale_controler.cs
@@ -19,6 +19,37 @@ namespace BoVeloManager.Classes {
             return temp;
         }
 
+        public List<Sale.displayInfo> GetSaleDisplayInfo_byClient(string Client_)
+        {
+            if (Client_ == "") { return GetSaleDisplayInfo(); }
+            else
+            {
+                List<Sale.displayInfo> temp = new List<Sale.displayInfo>();
+
+                foreach (Sale s in saleList)
+                {
+                    //private Client clt = s.getClient();
+
+                    Console.WriteLine(s.getClient().ToString());
+                    Console.WriteLine("////////////////////////////////////");
+                
+
+                    if (s.getClient().getFirstName() == Client_ || s.getClient().getLastName() == Client_ || s.getClient().getName() == Client_)
+                    {
+                        temp.Add(s.GetSaleDisplayInfo());
+                    }
+                
+                
+                }
+                return temp;
+            }
+            
+        }
+
+
+       
+
+
         public void createSale(Sale s) {
             saleList.Add(s);
             DatabaseClassInterface.addSale(s);
