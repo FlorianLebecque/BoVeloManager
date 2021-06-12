9c8e37bd80da55665a718bc53c00fd9710bfaf89
dRckt <damien.ryckaert@gmail.com>
2021-06-12 16:08:49 +0200

    .

--- BoVeloManager/Classes/Sale.cs
+++ BoVeloManager/Classes/Sale.cs
@@ -8,7 +8,7 @@ using BoVeloManager.Classes;
 namespace BoVeloManager.Classes {
     public class Sale : Transaction {
         
-        private List<Bike> bikeList;        
+        private List<Bike> bikeList;
 
         public Sale(int id_, int id_seller, int id_client, string state_, DateTime sale_date_, DateTime prevision_date_,List<Bike> bikeList_, List<User> userList, List<Client> clientList) : base(id_,id_seller,id_client,state_,sale_date_,prevision_date_,userList,clientList.Cast<Human>().ToList()) {
             
